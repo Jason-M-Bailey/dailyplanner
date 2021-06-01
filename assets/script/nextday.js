d462c850d95bc9575ae1ed4d88357e6dbac9b407
Jason Bailey <jmbailey816@gmail.com>
2021-06-01 13:56:10 -0400

    Code cleanup

--- assets/script/nextday.js
+++ assets/script/nextday.js
@@ -3,10 +3,10 @@ $(document).ready(function () {
     var value = $(this).siblings(".description").val();
     var time = $(this).parent().attr("id");
     localStorage.setItem(time, value);
-    $(".notification").addClass("show");
-    setTimeout(function () {
-      $(".notification").removeClass("show");
-    }, 2000);
+    // $(".notification").addClass("show");
+    // setTimeout(function () {
+    //   $(".notification").removeClass("show");
+    // }, 2000);
   });
 
   function hourUpdater() {
@@ -27,25 +27,25 @@ $(document).ready(function () {
   }
 
   hourUpdater();
-  var interval = setInterval(hourUpdater, 15000);
-  $("#hour-6 .description").val(localStorage.getItem("hour-6"));
-  $("#hour-7 .description").val(localStorage.getItem("hour-7"));
-  $("#hour-8 .description").val(localStorage.getItem("hour-8"));
-  $("#hour-9 .description").val(localStorage.getItem("hour-9"));
-  $("#hour-10 .description").val(localStorage.getItem("hour-10"));
-  $("#hour-11 .description").val(localStorage.getItem("hour-11"));
-  $("#hour-12 .description").val(localStorage.getItem("hour-12"));
-  $("#hour-13 .description").val(localStorage.getItem("hour-13"));
-  $("#hour-14 .description").val(localStorage.getItem("hour-14"));
-  $("#hour-15 .description").val(localStorage.getItem("hour-15"));
-  $("#hour-16 .description").val(localStorage.getItem("hour-16"));
-  $("#hour-17 .description").val(localStorage.getItem("hour-17"));
-  $("#hour-18 .description").val(localStorage.getItem("hour-18"));
-  $("#hour-19 .description").val(localStorage.getItem("hour-19"));
-  $("#hour-20 .description").val(localStorage.getItem("hour-20"));
-  $("#hour-21 .description").val(localStorage.getItem("hour-21"));
-  $("#hour-22 .description").val(localStorage.getItem("hour-22"));
-  $("#hour-23 .description").val(localStorage.getItem("hour-23"));
+  // var interval = setInterval(hourUpdater, 15000);
+  // $("#hour-6 .description").val(localStorage.getItem("hour-6"));
+  // $("#hour-7 .description").val(localStorage.getItem("hour-7"));
+  // $("#hour-8 .description").val(localStorage.getItem("hour-8"));
+  // $("#hour-9 .description").val(localStorage.getItem("hour-9"));
+  // $("#hour-10 .description").val(localStorage.getItem("hour-10"));
+  // $("#hour-11 .description").val(localStorage.getItem("hour-11"));
+  // $("#hour-12 .description").val(localStorage.getItem("hour-12"));
+  // $("#hour-13 .description").val(localStorage.getItem("hour-13"));
+  // $("#hour-14 .description").val(localStorage.getItem("hour-14"));
+  // $("#hour-15 .description").val(localStorage.getItem("hour-15"));
+  // $("#hour-16 .description").val(localStorage.getItem("hour-16"));
+  // $("#hour-17 .description").val(localStorage.getItem("hour-17"));
+  // $("#hour-18 .description").val(localStorage.getItem("hour-18"));
+  // $("#hour-19 .description").val(localStorage.getItem("hour-19"));
+  // $("#hour-20 .description").val(localStorage.getItem("hour-20"));
+  // $("#hour-21 .description").val(localStorage.getItem("hour-21"));
+  // $("#hour-22 .description").val(localStorage.getItem("hour-22"));
+  // $("#hour-23 .description").val(localStorage.getItem("hour-23"));
 
   $("#hour-30 .description").val(localStorage.getItem("hour-30"));
   $("#hour-31 .description").val(localStorage.getItem("hour-31"));
@@ -68,17 +68,13 @@ $(document).ready(function () {
   $("#tomorrow").text(moment().add(1, "days").format("dddd, MMMM Do")); // adds 1 day to moment.js
 });
 
+// DELETE APPOINTMENT & REFRESH PAGE
 $(document).ready(function () {
   $(".deleteBtn").on("click", function () {
     var value = $(this).siblings(".description").val();
     var time = $(this).parent().attr("id");
     localStorage.removeItem(time, value);
-
-    $(".notification").addClass("show");
-    setTimeout(function () {
-      $(".notification").removeClass("show");
-      location.reload();
-    }, 2000);
+    location.reload(); // refresh the page so the text disappears
   });
 });
 
